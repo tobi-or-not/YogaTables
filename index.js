e81e82a2372667359d058bf72d7ad9b0bc132ab3
tobias <mail@tobi-or-not.de>
2015-01-13 17:28:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict';
+module.exports = exports = {};
+
+var Table = require('./lib/view-lib');
+var collectionMixin = require('./lib/collection-mixin');
+
+exports.Table = Table;
+exports.collectionMixin = collectionMixin;
+
+
